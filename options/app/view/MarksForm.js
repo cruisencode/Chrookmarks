87bb987733f9b0b2650e473a220e2c3675501337
James Nuzzi <jim@jimnuzzi.com>
2013-01-18 12:19:35 -0500

    Changed name to Chrookmarks

--- options/app/view/MarksForm.js
+++ options/app/view/MarksForm.js
@@ -1,20 +1,20 @@
 /*
- This file is part of Chromarks.
+ This file is part of Chrookmarks.
 
- Copyright (c) 2012, James Nuzzi
+ Copyright (c) 2013, James Nuzzi
 
- Chromarks is free software: you can redistribute it and/or modify
+ Chrookmarks is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
 
- Chromarks is distributed in the hope that it will be useful,
+ Chrookmarks is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with Chromarks.  If not, see <http://www.gnu.org/licenses/>.
+ along with Chrookmarks.  If not, see <http://www.gnu.org/licenses/>.
  */
 Ext.define("options.view.MarksForm", {
   extend: 'Ext.panel.Panel',
--- options/app/view/Title.js
+++ options/app/view/Title.js
@@ -1,25 +1,25 @@
 /*
- This file is part of Chromarks.
+ This file is part of Chrookmarks.
 
- Copyright (c) 2012, James Nuzzi
+ Copyright (c) 2013, James Nuzzi
 
- Chromarks is free software: you can redistribute it and/or modify
+ Chrookmarks is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
 
- Chromarks is distributed in the hope that it will be useful,
+ Chrookmarks is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with Chromarks.  If not, see <http://www.gnu.org/licenses/>.
+ along with Chrookmarks.  If not, see <http://www.gnu.org/licenses/>.
  */
 Ext.define("options.view.Title", {
   extend: 'Ext.panel.Panel',
   alias: 'widget.optionsTitle',
-  width: 285,
+  width: 300,
   height: 84,
   padding: 8,
   border: false,
